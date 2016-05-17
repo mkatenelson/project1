e68bdfaf41ab65b064d7037b6cbb08592d5f5b5b
Kate Nelson <mkatenelson@gmail.com>
2016-05-17 16:22:52 -0700

    css animation on hover and temporary on click

---
+++ script/script.js
@@ -0,0 +1,12 @@
+
+
+function wasFlipped() {
+	var testClicks = document.getElementsByClassName("flip");
+	for(var i = 0; i < testClicks.length; i++){
+		console.log("element found"); // returns 16 elements
+		// testClicks.addEventListener
+		// 
+		// if <td> with class .flip has been clicked
+		// remove class .flip and add class .stayFlip
+	}
+}
